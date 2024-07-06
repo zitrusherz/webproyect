e4104cb51344fac9e2a3a4343735bfca127ea979
Christopher Andrés Euribe Euribe <zitrusherz@gmail.com>
2024-07-05 23:18:50 -0300

    Delete frontend/citric alexandria directory

--- frontend/citric alexandria/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import { origenSlice, usuarioSlice } from "./Slice";
-
-export default configureStore({
-    reducer:{
-        cart:origenSlice.reducer,
-        usuario:usuarioSlice.reducer
-    }
-})
